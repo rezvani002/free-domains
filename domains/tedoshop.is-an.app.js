8862dd3e72fe7ca25f0bf37b2f1b9e1a741fd4ef
rezvani002 <alireza.rezvani69@gmail.com>
2024-03-17 13:49:11 +0330

    add tedoshop.is-an.app to domains

---
+++ domains/tedoshop.is-an.app.js
@@ -0,0 +1,12 @@
+// for more details watch the file `./domains.d.ts`
+
+addSubDomain({
+  description: 'my first online shop project', // describe your project in this field
+  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: 'tedoshop', // desired subdomain name
+  owner: {
+    email: 'alireza.rezvani69@gmail.com',
+  },
+  record: {
+    CNAME: 'cl-gl1862aff7.gcdn.co', // e.g.: <your-github-account>.github.io
+  },
